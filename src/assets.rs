e3d2f4596777c9269b9cd69c185b6cfc991ec762
Manuel Quarneti <manuelquarneti@gmail.com>
2021-12-06 13:12:28 +0100

    refactoring

--- src/assets.rs
+++ src/assets.rs
@@ -2,6 +2,7 @@ use crate::util::{download_file, get_base_dir};
 use serde::Deserialize;
 use std::collections::HashMap;
 use std::error::Error;
+use std::path::PathBuf;
 
 #[derive(Deserialize)]
 struct Assets {
@@ -13,12 +14,18 @@ struct Object {
     hash: String,
 }
 
+fn get_objects_dir() -> Result<PathBuf, Box<dyn Error>> {
+    let path = get_base_dir()?
+        .join("assets")
+        .join("objects");
+
+    Ok(path)
+}
+
 fn download_asset(hash: &str) -> Result<(), Box<dyn Error>> {
     let first2 = &hash[..2];
 
-    let path = get_base_dir()?
-        .join("assets")
-        .join("objects")
+    let path = get_objects_dir()?
         .join(&first2)
         .join(&hash);
 
