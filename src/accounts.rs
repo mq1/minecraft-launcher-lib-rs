8a6272003e4693053140f9821063b9ce092efb54
Manuel Quarneti <manuelquarneti@gmail.com>
2022-01-08 14:54:50 +0100

    working authentication :D

--- src/accounts.rs
+++ src/accounts.rs
@@ -2,22 +2,22 @@ use serde::{Deserialize, Serialize};
 use std::{
     error::Error,
     fs,
-    path::{Path, PathBuf},
+    path::{Path, PathBuf}, thread, time::Duration,
 };
-
 use crate::util::get_base_dir;
 
 #[derive(Serialize, Deserialize)]
 pub struct Account {
-    name: String,
-    id: String,
     access_token: String,
+    ms_refresh_token: String
 }
 
 #[derive(Serialize, Deserialize)]
 struct Config {
     accounts: Vec<Account>,
 }
+
+const CLIENT_ID: &str = "2000ea79-d993-4591-b9c4-e678f82ae1db";
 
 fn get_config_path() -> Result<PathBuf, Box<dyn Error>> {
     let path = get_base_dir()?.join("accounts.toml");
@@ -64,11 +64,144 @@ pub fn list() -> Result<Vec<Account>, Box<dyn Error>> {
     Ok(config.accounts)
 }
 
-pub fn add(account: Account) -> Result<(), Box<dyn Error>> {
+fn add(account: Account) -> Result<(), Box<dyn Error>> {
     let mut config = read()?;
     config.accounts.push(account);
 
     write(&config)?;
+
+    Ok(())
+}
+
+// https://docs.microsoft.com/en-us/azure/active-directory/develop/v2-oauth2-device-code
+pub fn authorize_device() -> Result<(String, String, String), Box<dyn Error>> {
+    let resp: serde_json::Value = ureq::post("https://login.microsoftonline.com/consumers/oauth2/v2.0/devicecode")
+        .send_form(&[
+            ("client_id", CLIENT_ID),
+            ("scope", "XboxLive.signin%20offline_access")
+        ])?
+        .into_json()?;
+
+    let device_code = resp["device_code"].to_string();
+    let user_code = resp["user_code"].to_string();
+    let verification_uri = resp["verification_uri"].to_string();
+
+    Ok((device_code, user_code, verification_uri))
+}
+
+fn authenticate_with_xbl(ms_access_token: &str) -> Result<String, Box<dyn Error>> {
+    let resp: serde_json::Value = ureq::post("https://user.auth.xboxlive.com/user/authenticate")
+        .send_json(ureq::json!({
+            "Properties": {
+                "AuthMethod": "RPS",
+                "SiteName": "user.auth.xboxlive.com",
+                "RpsTicket": format!("d={}", ms_access_token)
+            },
+            "RelyingParty": "http://auth.xboxlive.com",
+            "TokenType": "JWT"
+        }))?
+        .into_json()?;
+
+    let xbl_token = resp["Token"].to_string();
+
+    Ok(xbl_token)
+}
+
+fn authenticate_with_xsts(xbl_token: &str) -> Result<(String, String), Box<dyn Error>> {
+    let resp: serde_json::Value = ureq::post("https://xsts.auth.xboxlive.com/xsts/authorize")
+        .send_json(ureq::json!({
+            "Properties": {
+                "SandboxId": "RETAIL",
+                "UserTokens": [
+                    xbl_token
+                ]
+            },
+            "RelyingParty": "rp://api.minecraftservices.com/",
+            "TokenType": "JWT"
+        }))?
+        .into_json()?;
+
+    let xsts_token = resp["Token"].to_string();
+    let user_hash = resp["DisplayClaims"]["xui"][0]["uhs"].to_string();
+
+    Ok((xsts_token, user_hash))
+}
+
+fn authenticate_with_minecraft(xsts_token: &str, user_hash: &str) -> Result<String, Box<dyn Error>> {
+    let resp: serde_json::Value = ureq::post("https://api.minecraftservices.com/authentication/login_with_xbox")
+        .send_json(ureq::json!({
+            "identityToken": format!("XBL3.0 x={};{}", user_hash, xsts_token)
+        }))?
+        .into_json()?;
+    
+    let mc_access_token = resp["access_token"].to_string();
+
+    Ok(mc_access_token)
+}
+
+fn get_minecraft_access_token(ms_access_token: &str) -> Result<String, Box<dyn Error>> {
+    let xbl_token = authenticate_with_xbl(ms_access_token)?;
+    let (xsts_token, user_hash) = authenticate_with_xsts(&xbl_token)?;
+    let mc_access_token = authenticate_with_minecraft(&xsts_token, &user_hash)?;
+
+    Ok(mc_access_token)
+}
+
+// https://docs.microsoft.com/en-us/azure/active-directory/develop/v2-oauth2-device-code
+pub fn authenticate(device_code: &str) -> Result<(), Box<dyn Error>> {
+    #[derive(Deserialize)]
+    struct AuthenticationErrorResponse {
+        error: String
+    }
+
+    #[derive(Deserialize)]
+    struct AuthenticationResponse {
+        access_token: String,
+        refresh_token: String,
+    }
+
+    loop {
+        let auth = ureq::post("https://login.microsoftonline.com/consumers/oauth2/v2.0/token")
+            .send_form(&[
+                ("grant_type", "urn:ietf:params:oauth:grant-type:device_code"),
+                ("client_id", CLIENT_ID),
+                ("device_code", device_code)
+            ]);
+
+        match auth
+        {
+            Ok(response) => {
+                let resp: AuthenticationResponse = response.into_json()?;
+                let mc_access_token = get_minecraft_access_token(&resp.access_token)?;
+
+                add(Account {
+                    access_token: mc_access_token,
+                    ms_refresh_token: resp.refresh_token
+                })?;
+
+                break;
+            },
+            Err(ureq::Error::Status(_code, response)) => {
+                let resp: AuthenticationErrorResponse = response.into_json()?;
+                match resp.error.as_str() {
+                    "authorization_pending" => {
+                        thread::sleep(Duration::from_secs(5))
+                    },
+                    _ => {
+                        println!("Authentication error");
+                        // TODO handle other errors
+                        // https://docs.microsoft.com/en-us/azure/active-directory/develop/v2-oauth2-device-code#expected-errors
+
+                        break;
+                    }
+                }
+            }
+            Err(_) => {
+                println!("Network Error");
+                return Err(Box::new(auth.err().unwrap()));
+            }
+        }
+    }
 
     Ok(())
 }
