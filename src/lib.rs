72f73198cbdbf8ce3304840a15a9a9c262e51d77
Manuel Quarneti <manuelquarneti@gmail.com>
2022-01-09 13:58:23 +0100

    create app dir

--- src/lib.rs
+++ src/lib.rs
@@ -16,10 +16,16 @@ pub mod libraries;
 extern crate lazy_static;
 
 lazy_static! {
-    pub static ref BASE_DIR: PathBuf = ProjectDirs::from("eu", "mq1", "runmc")
-        .unwrap()
-        .data_dir()
-        .to_path_buf();
+    pub static ref BASE_DIR: PathBuf = {
+        let dir = ProjectDirs::from("eu", "mq1", "runmc")
+            .unwrap()
+            .data_dir()
+            .to_path_buf();
+
+            fs::create_dir_all(&dir).unwrap();
+
+        dir
+    };
 }
 
 pub fn download_file(url: &Url, path: &Path) -> Result<(), Box<dyn Error>> {
