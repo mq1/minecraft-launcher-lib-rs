546cdee3d503d22448a47d922b0218e0a20dce7a
Manuel Quarneti <manuelquarneti@gmail.com>
2022-01-09 09:33:16 +0100

    refactoring

--- src/lib.rs
+++ src/lib.rs
@@ -8,12 +8,3 @@ pub mod util;
 
 #[macro_use]
 extern crate lazy_static;
-
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
-    }
-}
