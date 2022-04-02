7e75163fbf8f5659f3a7b4c2098778a89bf54c14
Manuel Quarneti <manuelquarneti@gmail.com>
2022-04-02 18:07:54 +0200

    implement get_installed_versions

--- src/utils.rs
+++ src/utils.rs
@@ -1,4 +1,4 @@
-use std::path::PathBuf;
+use std::{path::{PathBuf, Path}, fs::{self, File}, io::BufReader};
 
 use anyhow::Result;
 use isahc::ReadResponseExt;
@@ -47,4 +47,26 @@ pub fn get_version_list() -> Result<Vec<Version>> {
     let version_manifest = isahc::get(VERSION_MANIFEST_URL)?.json::<VersionManifest>()?;
 
     Ok(version_manifest.versions)
+}
+
+/// Returns all installed versions
+pub fn get_installed_versions(minecraft_directory: &Path) -> Result<Vec<Version>> {
+    let versions_dir = &minecraft_directory.join("versions");
+    let dir_list = fs::read_dir(versions_dir)?;
+
+    let mut version_list = vec![];
+    for entry in dir_list {
+        let path = entry?.path();
+        let path = versions_dir.join(&path).join(&path).with_extension("json");
+
+        if path.is_file() {
+            let file = File::open(path)?;
+            let reader = BufReader::new(file);
+
+            let version_data = serde_json::from_reader(reader)?;
+            version_list.push(version_data);
+        }
+    }
+
+    Ok(version_list)
 }
