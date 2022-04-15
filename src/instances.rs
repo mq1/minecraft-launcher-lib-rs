21095574c740c91463959a8a1bfdf14fde243235
Manuel Quarneti <manuelquarneti@gmail.com>
2022-04-15 16:40:56 +0200

    ✨ Implement instances::rename

--- src/instances.rs
+++ src/instances.rs
@@ -9,3 +9,12 @@ pub fn new(name: &str, instances_dir: &Path) -> Result<PathBuf> {
 
     Ok(path)
 }
+
+/// renames an instance and returns its path
+pub fn rename(old_name: &str, new_name: &str, instances_dir: &Path) -> Result<PathBuf> {
+    let old_path = instances_dir.join(old_name);
+    let new_path = instances_dir.join(new_name);
+    fs::rename(old_path, &new_path)?;
+
+    Ok(new_path)
+}
